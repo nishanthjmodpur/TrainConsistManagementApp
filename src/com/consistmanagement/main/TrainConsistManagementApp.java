aba968fc2970950fd708f6ee5ee1cbbdcd94e70d
nishanthjmodpur <nishanthjmodpur@gmail.com>
2026-03-10 15:06:20 +0530

    Coded UC2 to support CRUD operations

--- src/com/consistmanagement/main/TrainConsistManagementApp.java
+++ src/com/consistmanagement/main/TrainConsistManagementApp.java
@@ -2,45 +2,92 @@ package com.consistmanagement.main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * =================================================================================
- *  MAIN CLASS - USE CASE 1 - TRAIN CONSIST MANAGEMENT APP 
+ * MAIN CLASS - USE CASE 2 - TRAIN CONSIST MANAGEMENT APP
  * =================================================================================
  * 
- * Use Case 1: Initialize Train and Display Consist Summary
+ * Use Case 2: Add Passenger Bogies to Train
  * 
- * Description:
- *  This class represents the entry point of the train consist management application.
- *  
- *  At this stage, the application:
- *  	- Creates an empty train consist
- *  	- Uses a dynamic list to store bogies
- *  	- Display initial bogie count
- *  	- Prints the current state of the train
- *  
- *  This use case introduces collection initialization and basic program
- *  startup flow.
- *  
- *  @author Development
- *  @version 1.0
+ * Description: This class demonstrates how passenger bogies can be managed
+ * dynamically using ArrayList operations.
+ * 
+ * At this stage, the application: - Adds new Bogies to the train - Removes
+ * existing bogies - Checks for bogie availability - Displays the final consist
+ * 
+ * This maps CRUD operations to ArrayList
+ * 
+ * @author Development
+ * @version 2.0
  */
-
 
 public class TrainConsistManagementApp {
 	/**
-	 * Main entry point for UC1
+	 * Main entry point for UC2
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("======================================");
 		System.out.println(" === Train Consist Management App === ");
 		System.out.println("======================================");
-		
+
 		List<String> trainConsist = new ArrayList<String>();
-		
+
 		System.out.println("Train initialized successfully.....");
 		System.out.println("Initial Bogie Count: " + trainConsist.size());
+
+		Scanner scanner = new Scanner(System.in);
+		String choice;
+
+		do {
+			System.out.println("1. Add Bogies");
+			System.out.println("2. Remove Bogies");
+			System.out.println("3. Check if Bogie Exists");
+			System.out.println("4. Display Consists");
+			System.out.println("5. Exit");
+			System.out.print("Enter your Choice: ");
+
+			choice = scanner.nextLine();
+
+			switch (choice) {
+				case "1" -> {
+					System.out.println("Adding Bogies");
+					System.out.println("Enter bogie name to add: ");
+					String bogie = scanner.nextLine();
+					trainConsist.add(bogie);
+				}
+				case "2" -> {
+					System.out.println("Enter bogie name to remove: ");
+					String bogie = scanner.nextLine();
+					if (trainConsist.contains(bogie)) {
+						trainConsist.remove(bogie);
+					} else {
+						System.out.println("Bogie doesn't exist");
+					}
+				}
+				case "3" -> {
+					System.out.println("Enter bogie name: ");
+					String bogie = scanner.nextLine();
+					if (trainConsist.contains(bogie)) {
+						System.out.println("Bogie exists");
+					} else {
+						System.out.println("Bogie doesn't exist");
+					}
+				}
+				case "4" -> {
+					System.out.println("Displaying consist");
+					System.out.println(trainConsist);
+				}
+				case "5" -> {
+					System.out.println("EXITING");
+				}
+			}
+		} while (!choice.equals("5"));
 		
+		
+		scanner.close();
 	}
 }
